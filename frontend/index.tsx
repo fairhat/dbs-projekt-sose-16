c8cd3d9095b8b46073463d044cb8facc023bea18
Ferhat Topcu <topcu.ft@gmail.com>
2016-07-12 19:25:50 +0200

    deleted old files

--- frontend/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { observable } from 'mobx'
-import { observer } from 'mobx-react'
-import DevTools from 'mobx-react-devtools'
